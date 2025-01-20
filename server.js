a543f9938e9f4aec664cf318e0a44c456ee87389
Maksum Grechka <mimaxyk@gmail.com>
2025-01-20 09:33:43 +0200

    js.code

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Роут для головної сторінки
 app.get('/', (req, res) => {
     const userIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(`IP-адреса користувача: ${userIP}`);
-    res.send(`Ваш IP-адреса: ${userIP}`);
+    
+    res.send(`venom`);
 });
 
 app.listen(port, () => {
